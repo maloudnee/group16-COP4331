7ee02c8c2c78161bdf5f3853dc2771e521a5ff96
Lazaro <lazaroalfonso145@gmail.com>
2026-02-17 15:41:44 -0500

    Removed commented out code from AddContact.php

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -17,28 +17,13 @@
 	}
 	else
 	{
-		// $stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=?");
-		// $stmt->bind_param("s", $inData["login"]);
-		// $stmt->execute();
-		// $result = $stmt->get_result();
+		$stmt = $conn->prepare("INSERT INTO Contacts (FirstName, LastName, Phone, Email, UserID) VALUES (?, ?, ?, ?, ?)");
 
-		// if( $row = $result->fetch_assoc()  )
-		// {
-        //     returnWithError("A user with this login already exists");
-        //     exit();
-		// }
-		// else
-		// {
-            // $stmt->close();
-			$stmt = $conn->prepare("INSERT INTO Contacts (FirstName, LastName, Phone, Email, UserID) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssi", $inData["FirstName"], $inData["LastName"], $inData["Phone"], $inData["Email"], $inData["ID"]);
 
-            $stmt->bind_param("ssssi", $inData["FirstName"], $inData["LastName"], $inData["Phone"], $inData["Email"], $inData["ID"]);
-
-            $stmt->execute();
-            $ID = $conn->insert_id;
-            returnWithInfo($inData["FirstName"], $inData["LastName"], $ID);
-		// }
-
+        $stmt->execute();
+        $ID = $conn->insert_id;
+        returnWithInfo($inData["FirstName"], $inData["LastName"], $ID);
 		$stmt->close();
 		$conn->close();
 	}
